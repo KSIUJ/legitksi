58ed1dab871fd402e799f6ecc8f612f1ce7b98b0
root <root@vps200111.ovh.net>
2015-11-14 22:45:17 +0100

    size fixes, auto page brake removed, date fix

--- web/legitksi.php
+++ web/legitksi.php
@@ -8,12 +8,17 @@ $pdf->SetCreator("Legitk(s)i");
 $pdf->SetFontSubsetting(false);
 $pdf->SetPrintFooter(false);
 $pdf->SetPrintHeader(false);
+$pdf->SetAutoPageBreak(TRUE, 0);
 		
 /* SIZE CONFIG - in mm */
 $leftMargin=5;$topMargin=5;
 $cellsPerPage=10;
+<<<<<<< HEAD
+$cellWidth=80; $cellHeight=50;
+=======
 //$cellWidth=85; $cellHeight=54; //<-- -1mm for border (A4 is 297mm heigh)
 $cellWidth=78; $cellHeight=49;
+>>>>>>> 5bf5c56f94cf1089be6fed56cfad1cfc44fe651c
 
 $handle = fopen("config/ludzie.txt", "r");
 if ($handle) {
@@ -32,12 +37,12 @@ if ($handle) {
 		//border
 		$pdf->MultiCell($cellWidth, $cellHeight, "", 1, 'C');
 		//name
-		$pdf->SetY($y+30);$pdf->SetX($x);
+		$pdf->SetY($y+27.5);$pdf->SetX($x);
 		$pdf->SetFont('freemono', '', 18);
 		$pdf->MultiCell($cellWidth, 20, $line, 0, 'C');
 		//footer
 		$pdf->SetFont('freemono', '', 9);
-		$pdf->SetY($y+46);$pdf->SetX($x);
+		$pdf->SetY($y+42.5);$pdf->SetX($x);
 		$pdf->MultiCell($cellWidth, 20, $footer, 0, 'C', 0, 0, '', '', true, 0, true);
 		
 		if ($i%2==1){$y+=$cellHeight;} //row change
